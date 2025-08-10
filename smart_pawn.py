564bc6af42b14c6216983d5c42b85f162d991d70
morteza <motteza1182@gmail.com>
2025-08-10 13:06:56 +0330

    Creat main_df in __init__

--- smart_pawn.py
+++ smart_pawn.py
@@ -2,5 +2,15 @@ import pandas as pd
 import numpy as np
 
 class SmartPawn:
-    def __init__(self):
-        pass
+    aggressive = {}
+    kind = {}
+    Strategy = {}
+    def __init__(self, player_mode):
+        pawns = [F"p{i}_pawn{j}" for i in range(player_mode) for j in range(4)]
+        pawns_mode = [mode for mode in player_mode for _ in range(4)]
+        initial_loc = ["not_in_game" for _ in range(0, player_mode*4)]
+        self.main_df = pd.DataFrame({
+            "pawn_id": pawns,
+            "mode": pawns_mode,
+            "loc": initial_loc
+        })
